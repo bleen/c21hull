e9c1d6a0d9a6e382cc8a8a8c3bde1a1270479601
bleen <git.119x@bleen.net>
2013-06-04 23:24:17 -0400

    Initial commit

---
+++ settings.local.php
@@ -0,0 +1,17 @@
+<?php
+
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'c21hull',
+      'username' => 'root',
+      'password' => 'root',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
