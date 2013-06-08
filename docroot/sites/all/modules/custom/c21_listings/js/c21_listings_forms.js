76c9993762434501c75697a20d839042445a3777
bleen <git.119x@bleen.net>
2013-06-08 09:15:57 -0400

    Improving the listings form

---
+++ docroot/sites/all/modules/custom/c21_listings/js/c21_listings_forms.js
@@ -0,0 +1,28 @@
+(function ($) {
+  Drupal.behaviors.features = {
+    attach: function(context, settings) {
+      $(':input[name="field_listing_comission[und][0][value]"]').siblings('.field-prefix').hide();
+      $(':input[name="field_listing_comission[und][0][value]"]').keyup(function (event) {
+        if ($(this).val() <= 100) {
+          $(this).siblings('.field-prefix').hide();
+          $(this).siblings('.field-suffix').show();
+        }
+        else {
+          $(this).siblings('.field-prefix').show();
+          $(this).siblings('.field-suffix').hide();
+        }
+      });
+    }
+  };
+})(jQuery);
+
+
+
+
+
+
+
+
+
+
+
