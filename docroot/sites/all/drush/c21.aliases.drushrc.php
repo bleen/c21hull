eb0e50acbc97f00de05c510842fea0eca4285c1b
bleen <git.119x@bleen.net>
2013-06-14 14:55:14 -0400

    Adding a drush alias file

---
+++ docroot/sites/all/drush/c21.aliases.drushrc.php
@@ -0,0 +1,23 @@
+<?php
+/* ==========================  L O C A L  =================================== */
+$aliases['local'] = array(
+  'parent' => '@parent',
+  'env' => 'local',
+  'ssh-options' => '-o StrictHostKeyChecking=no',
+  'sites-subdir' => 'default',
+  'uri' => 'http://local.c21hull:8888',
+);
+
+/* ==========================  R E M O T E ============== */
+$aliases['stage'] = array(
+  'env' => 'stage',
+  'remote-user' => 'hull',
+  'remote-host' => 'c21hull.com',
+  'parent' => '@c21.local',
+  'path-aliases' => array(
+    '%drush-script' => '/home/hull/pear/drush',
+    '%dump-dir' => '/home/hull/tmp'
+  ),
+  'root' => '/home/hull/sites/c21hull.com/docroot',
+  'uri' => 'http://d7.c21hull.com',
+);
