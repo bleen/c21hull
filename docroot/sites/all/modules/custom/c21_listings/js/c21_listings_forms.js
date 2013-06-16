87f21c190bd9e4f56c3bdfbe506c48208a7171bc
bleen <git.119x@bleen.net>
2013-06-15 23:25:41 -0400

    Fixing spelling error in field name and other minor tweaks

--- docroot/sites/all/modules/custom/c21_listings/js/c21_listings_forms.js
+++ docroot/sites/all/modules/custom/c21_listings/js/c21_listings_forms.js
@@ -1,8 +1,8 @@
 (function ($) {
   Drupal.behaviors.features = {
     attach: function(context, settings) {
-      $(':input[name="field_listing_comission[und][0][value]"]').siblings('.field-prefix').hide();
-      $(':input[name="field_listing_comission[und][0][value]"]').keyup(function (event) {
+      $(':input[name="field_listing_commission[und][0][value]"]').siblings('.field-prefix').hide();
+      $(':input[name="field_listing_commission[und][0][value]"]').keyup(function (event) {
         if ($(this).val() <= 100) {
           $(this).siblings('.field-prefix').hide();
           $(this).siblings('.field-suffix').show();
