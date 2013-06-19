47fbeb8b3c2fd035fd2e76166ce06c19e65ed55f
bleen <git.119x@bleen.net>
2013-06-19 15:51:00 -0400

    Adding the ability to hide/show former agents on the lisitng form

--- docroot/sites/all/modules/custom/c21_listings/js/c21_listings_forms.js
+++ docroot/sites/all/modules/custom/c21_listings/js/c21_listings_forms.js
@@ -1,6 +1,31 @@
 (function ($) {
   Drupal.behaviors.features = {
     attach: function(context, settings) {
+      // Hide the "former" agents and add a "show former agents" link.
+      $.get('/ajax/agents', function(agents) {
+        $('select[name^="field_listing_agent"] option:not(:selected)').each(function(context) {
+          if (agents[$(this).val()]['status'] === 'former') {
+            $(this).remove();
+          }
+        });
+
+        $('select[name^="field_listing_agent"]').after('<a href="" class="show-agents">Show former agents</a>');
+        $('.show-agents').click(function(event) {
+          event.preventDefault();
+
+          for (var aid in agents) {
+            if (agents[aid]['status'] === 'former') {
+              $('select[name^="field_listing_agent"]').append(
+                $("<option></option>").attr("value", aid).text(agents[aid]['full_name'])
+              );
+              $('select[name^="field_listing_agent"]').height("150");
+              $('.show-agents').remove();
+            }
+          }
+        });
+      });
+
+
       // Show/Hide the prefix/suffix for commission based on its current value.
       $(':input[name="field_listing_commission[und][0][value]"]').siblings('.field-prefix').hide();
       $(':input[name="field_listing_commission[und][0][value]"]').keyup(function (event) {
