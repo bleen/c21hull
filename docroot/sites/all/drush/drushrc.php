c39539a5d5598df1c0eb7967a27d157ca5a4c56e
bleen <git.119x@bleen.net>
2013-06-27 23:05:43 -0400

    Adding a drush env command

--- docroot/sites/all/drush/drushrc.php
+++ docroot/sites/all/drush/drushrc.php
@@ -18,3 +18,57 @@ $command_specific['site-install'] = array(
   'account-mail' => 'admin@c21hull.com',
 );
 $command_specific['c21-site-install'] = $command_specific['site-install'];
+
+// Define settings needed by the env command.
+$enable_modules = array(
+  'c21' => 1,
+  'c21_listings' => 1,
+  'c21_migration' => 1,
+  'syslog' => 1,
+);
+$disable_modules = array(
+  'color' => 0,
+  'comment' => 0,
+  'dblog' => 0,
+  'toolbar' => 0,
+);
+$options['environments'] = array(
+  'dev' => array(
+    // The list of modules to enabled (1) or disable (0).
+    'modules' => array(
+      'views_ui' => 1,
+      'context_ui' => 1,
+      'devel' => 1,
+      'devel_generate' => 1,
+      'ds_ui' => 1,
+      'search_krumo' => 1,
+      'testing' => 1,
+    ),
+    // The list of variables to configure.
+    'settings' => array(
+      'preprocess_css' => 0,
+      'preprocess_js' => 0,
+    ),
+  ),
+  'prod' => array(
+    // The list of modules to enabled (1) or disable (0).
+    'modules' => array(
+      'views_ui' => 0,
+      'context_ui' => 0,
+      'devel' => 0,
+      'devel_generate' => 0,
+      'ds_ui' => 0,
+      'search_krumo' => 0,
+      'testing' => 0,
+    ),
+    // The list of variables to configure.
+    'settings' => array(
+      'preprocess_css' => 1,
+      'preprocess_js' => 1,
+    ),
+  ),
+);
+
+foreach ($options['environments'] as $env => $settings) {
+  $options['environments'][$env]['modules'] = array_merge($options['environments'][$env]['modules'], $enable_modules, $disable_modules);
+}
