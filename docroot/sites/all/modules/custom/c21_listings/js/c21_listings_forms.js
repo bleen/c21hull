1035007b9ba13ff58ae2edb14c8431e154937336
bleen <git.119x@bleen.net>
2013-08-06 22:19:36 -0400

    Adding vertical tab messages to listings

--- docroot/sites/all/modules/custom/c21_listings/js/c21_listings_forms.js
+++ docroot/sites/all/modules/custom/c21_listings/js/c21_listings_forms.js
@@ -1,6 +1,42 @@
 (function ($) {
+  Drupal.c21 = Drupal.c21 || {};
+
   Drupal.behaviors.c21_listings = {
     attach: function(context, settings) {
+      // Provide the vertical tab summaries.
+      fieldset = $('.group-listing-transaction-dtls', context);
+      fields = {'field-listing-closing-date':'text', 'field-listing-sale-price':'text', 'field-listing-gross-commission':'text'};
+      Drupal.c21.drupalSetSummary(fieldset, fields);
+
+      fieldset = $('.group-listing-general-info', context);
+      fields = {'field-listing-type':'select', 'field-listing-list-date':'date range', 'field-listing-zoning':'select', 'field-listing-list-price':'text'};
+      Drupal.c21.drupalSetSummary(fieldset, fields);
+
+      fieldset = $('.group-listing-building-info', context);
+      fields = {'field-listing-building-type':'select', 'field-listing-bedrooms':'text', 'field-listing-bathrooms':'text'};
+      Drupal.c21.drupalSetSummary(fieldset, fields);
+
+      fieldset = $('.group-listing-measurements', context);
+      fields = {'field-listing-sq-ft-above':'text', 'field-listing-sq-ft-below':'text', 'field-listing-sq-ft-finished':'text'};
+      Drupal.c21.drupalSetSummary(fieldset, fields);
+
+      fieldset = $('.group-listing-location-info', context);
+      fields = {'field-listing-township':'text', 'field-listing-development':'text', 'field-listing-school-district':'text'};
+      Drupal.c21.drupalSetSummary(fieldset, fields);
+
+      fieldset = $('.group-listing-buyers', context);
+      fields = {'buyer':'person'};
+      Drupal.c21.drupalSetSummary(fieldset, fields);
+
+      fieldset = $('.group-listing-sellers', context);
+      fields = {'seller':'person'};
+      Drupal.c21.drupalSetSummary(fieldset, fields);
+
+      $('.group-listing-images-videos', context).drupalSetSummary(function(context) {
+        return '<img src="' + $('.field-name-field-listing-featured-photo img').attr('src') + '" />';
+      });
+
+
       // Hide the "former" agents and add a "show former agents" link.
       $.get('/ajax/agents', function(agents) {
         $('.field-agents .form-checkboxes .form-item input:not(:checked)').each(function(context) {
@@ -56,10 +92,60 @@
       $(':input[name="field_listing_commission[und][0][value]"]').keyup();
     }
   };
+
+  Drupal.c21.drupalSetSummary = function(fieldset, fields) {
+    fieldset.drupalSetSummary(function(context) {
+      var vals = [];
+
+      $.each(fields, function(key, value) {
+        label = $('.field-name-' + key).find('label').first().text().replace(/\*$/, "").trim();
+
+        switch(value) {
+          case 'text':
+            field = $('.field-name-' + key).find('input').first().val();
+            break;
+          case 'select':
+            field = $('.field-name-' + key).find(':selected').first().text();
+            break;
+          case 'date range':
+            field = [];
+            $('.field-name-' + key).find('input').each(function(context){
+              field.push($(this).val());
+            });
+            field = field.join(' to ');
+            break;
+          case 'person':
+            $('.field-name-field-listing-' + key + ' #field-listing-' + key + '-values > tbody > tr').each(function(context) {
+              field = $(this).find('.field-name-field-' + key + '-first-name input').val() + ' ';
+              field += $(this).find('.field-name-field-' + key + '-last-name input').val();
+            });
+            break;
+          default:
+            field = '';
+        }
+
+        vals.push(label === '' ? field : label + ': ' + field);
+      });
+
+      return vals.join('<br/>');
+    });
+  };
+
 })(jQuery);
 
 
 
+
+$('.group-listing-buyers', context).drupalSetSummary(function(context) {
+        var vals = [];
+        $('.field-name-field-listing-buyer #field-listing-buyer-values > tbody > tr').each(function(context) {
+          fullname = $(this).find('.field-name-field-buyer-first-name input').val() + ' ';
+          fullname += $(this).find('.field-name-field-buyer-last-name input').val();
+          vals.push(fullname);
+        });
+
+        return vals.join('<br/>');
+      });
 
 
 
