5af89e7985574e2a6820a3302af85d9f1cd76ce6
bleen <git.119x@bleen.net>
2013-08-10 06:31:43 -0400

    improving the build scripts to accept arguments

--- builds/install.sh
+++ builds/install.sh
@@ -1,7 +1,16 @@
+#!/bin/sh
+
+T="$(date +%s)"
+
+env = ${1:-dev}
+
 drush c21si -y
-drush env dev -y
+drush env $env -y
 drush en c21_rets_connection -y
 drush cc all
 drush dis c21_rets_connection -y
 drush fra --force -y
 drush cc all
+
+T="$(($(date +%s)-T))"
+echo "The install script took ${T} seconds"
--- builds/migrate.sh
+++ builds/migrate.sh
@@ -1,6 +1,15 @@
+#!/bin/sh
+
+T="$(date +%s)"
+
+count = ${1:-5}
+
 drush env migration -y
 drush mreg
 drush mi c21User
 drush mi --group=listing_vocabularies
 drush mi c21Agent
-drush mi c21Listing --limit="5 items"
+drush mi c21Listing --limit="${count} items"
+
+T="$(($(date +%s)-T))"
+echo "The migration script took ${T} seconds (${count} listings)"
