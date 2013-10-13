f487e5786710af12f328730854b847e44822958b
bleen <git.119x@bleen.net>
2013-10-13 10:02:22 -0400

    Adding longitude and latitude fields in case the map is innacurate

--- docroot/sites/all/modules/custom/c21_listings/js/c21_listings_maps.js
+++ docroot/sites/all/modules/custom/c21_listings/js/c21_listings_maps.js
@@ -12,10 +12,12 @@
     geocoder.geocode( { 'address': listing_address}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
         mapOptions = {
-          zoom: 8,
+          zoom: 12,
           center: results[0].geometry.location,
           mapTypeId: google.maps.MapTypeId.ROADMAP,
-          zoomControl: true
+          panControl: false,
+          zoomControl: true,
+          scaleControl: false
         }
         var map = new google.maps.Map(document.getElementById('listing-map'), mapOptions);
         var marker = new google.maps.Marker({
